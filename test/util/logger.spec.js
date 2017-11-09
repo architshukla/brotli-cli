99532865b2a388da112f0034184acbb6cfc7096e
Archit Shukla <architshukla@live.com>
2017-11-09 23:05:02 +0530

    refactor(logger): remove duplicate code from logger tests

--- test/util/logger.spec.js
+++ test/util/logger.spec.js
@@ -21,39 +21,31 @@ describe('logger', () => {
     sandbox.restore();
   });
 
+  function loggerTestHelper(methodName, logLevel) {
+    const consoleMethodSpy = sandbox.stub(console, methodName);
+
+    logger[methodName](testMessage);
+
+    assert.ok(consoleMethodSpy.calledOnce, `console.${methodName}() was not called once`);
+    [pkg.name, logLevel, testMessage]
+      .forEach(m => assert.ok(consoleMethodSpy.args[0][0].indexOf(m) !== -1, `'${m}' was expected to be logged`));
+  }
+
   describe('#info()', () => {
     it('should log an info message with package name and log level', () => {
-      const consoleInfoSpy = sinon.stub(console, 'info');
-
-      logger.info(testMessage);
-
-      assert.ok(consoleInfoSpy.calledOnce, 'console.info() was not called once');
-      [pkg.name, infoLogLevel, testMessage]
-        .forEach(m => assert.ok(consoleInfoSpy.args[0][0].indexOf(m) !== -1, `'${m}' was expected to be logged`));
+      loggerTestHelper('info', infoLogLevel);
     });
   });
 
   describe('#warn()', () => {
     it('should log a warning with package name and log level', () => {
-      const consoleWarnSpy = sinon.stub(console, 'warn');
-
-      logger.warn(testMessage);
-
-      assert.ok(consoleWarnSpy.calledOnce, 'console.warn() was not called once');
-      [pkg.name, warnLogLevel, testMessage]
-        .forEach(m => assert.ok(consoleWarnSpy.args[0][0].indexOf(m) !== -1, `'${m}' was expected to be logged`));
+      loggerTestHelper('warn', warnLogLevel);
     });
   });
 
   describe('#error()', () => {
     it('should log an error with package name and log level', () => {
-      const consoleErrorSpy = sinon.stub(console, 'error');
-
-      logger.error(testMessage);
-
-      assert.ok(consoleErrorSpy.calledOnce, 'console.error() was not called once');
-      [pkg.name, errorLogLevel, testMessage]
-        .forEach(m => assert.ok(consoleErrorSpy.args[0][0].indexOf(m) !== -1, `'${m}' was expected to be logged`));
+      loggerTestHelper('error', errorLogLevel);
     });
   });
 });
